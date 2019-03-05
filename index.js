684ac813961e4cfa6d1a5afdafbaa68b1cfa8a95
radhe-shyam <jangir.radheyshyam@gmail.com>
2019-03-05 01:47:33 -0500

    express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+app.get('/', (req, res) => {
+    res.send('Welcome');
+});
+
+app.listen(port, () => {
+    console.log('Server started on:', port);
+})
+
