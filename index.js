f1857e854ec5467b87914160ee8bb853bb47cc3d
radhe-shyam <jangir.radheyshyam@gmail.com>
2019-03-06 06:36:30 -0500

    express-session connet-flash alert for actions

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 //load models
 require('./models/Idea');
@@ -34,6 +36,25 @@ app.use(bodyParser.json());
 //_method override middleware
 
 app.use(methodOverride('_method'));
+
+// Express session middleware
+
+app.use(session({
+    secret: 'tadadadada',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// flash middleware
+app.use(flash());
+
+//global variables
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 //request methods
 app.get('/', (req, res) => {
@@ -90,6 +111,7 @@ app.post('/ideas', (req, res) => {
         new Idea(newUser)
             .save()
             .then(idea => {
+                req.flash('success_msg', 'Idea added');
                 res.redirect('/ideas');
             })
     }
@@ -103,15 +125,17 @@ app.put('/ideas/:id', (req, res) => {
         idea.details = req.body.details;
         return idea.save();
     }).then(() => {
+        req.flash('success_msg', 'Idea updated');
         res.redirect('/ideas');
     })
 });
 
 app.delete('/ideas/:id', (req, res) => {
     Idea.deleteOne({ _id: req.params.id })
-    .then(() => {
-        res.redirect('/ideas');
-    });
+        .then(() => {
+            req.flash('error_msg', 'Idea removed');
+            res.redirect('/ideas');
+        });
 })
 
 app.listen(port, () => {
