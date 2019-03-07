b473a31dd8a60d431c49877e0a95fa8832b53c44
radhe-shyam <jangir.radheyshyam@gmail.com>
2019-03-07 01:49:15 -0600

    users schema

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+require('../models/User');
+const User = mongoose.model('users');
 
 router.get('/login', (req, res) => {
     res.render('users/login');
