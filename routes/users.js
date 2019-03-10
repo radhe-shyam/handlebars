2fbd2c88eeda6afc8c5cac1e22151ec540ad3472
radhe-shyam <jangir.radheyshyam@gmail.com>
2019-03-10 08:30:24 -0500

    user register

--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,39 @@ router.get('/login', (req, res) => {
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
+
+router.post('/', (req, res) => {
+    let errors = [];
+
+    if (!req.body.name) {
+        errors.push({ text: "Please provide the name" });
+    }
+
+    if (!req.body.email) {
+        errors.push({ text: 'Please provide the email' });
+    }
+
+    if (!req.body.password) {
+        errors.push({ text: 'Please provide the password' })
+    }
+
+    if (!req.body.password2) {
+        errors.push({ text: "Please provide the confirm password" });
+    }
+
+    if (req.body.password && req.body.password2 && req.body.password != req.body.password2) {
+        errors.push({ text: 'Password and confirm password should be same.' });
+    }
+
+    if (errors.length > 0) {
+        res.render('users/register', {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            password2: req.body.password2
+        })
+    } else
+        res.send('register');
+});
 module.exports = router;
